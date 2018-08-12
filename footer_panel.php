dd1b77fc7efa9f06f3794b382e71b7fa84aaf56a
pallavi-ghimire <pallavi.ghimire.39@gmail.com>
2018-08-12 11:54:50 +0545

    Final commit

---
+++ footer_panel.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="myFunction.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<style>
+    @import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    #nav ul.social li{
+        padding: 5px 5px;
+    }
+    .col-sm ul.social li a i {
+        margin-right: 5px;
+        font-size:15px;
+        -webkit-transition: .3s all ease;
+        -moz-transition: .3s all ease;
+        transition: .3s all ease;
+    }
+    .col-sm ul.social li:hover a i {
+        font-size:18px;
+    }
+    .col-sm ul.social li a,
+    .col-sm ul.quick-links li a{
+        color:#ffffff;
+    }
+    .col-sm ul.social li a:hover{
+        color:#eeeeee;
+    }
+
+</style>
+<!-- // Footer Top Row -->
+<div class="row box" style="background:#404040; color:#FFF; padding-top: 5%; padding-bottom: 5%">
+    <div class="col-sm-3 col-md-3">
+        <div class="box transparent">
+            <div class="box-header" style="padding-left: 10%">
+                <h2 class="titlefooter">Institute</h2>
+            </div>
+            <div class="box-body1">
+                <style>
+                    .box-body1 a:link{
+                        color: white;
+                    }
+                    .box-body1 a:visited{
+                        color: white;
+                    }
+                </style>
+                <ul class="list-item" style="list-style-type: none">
+                    <li><a href="/enterprise/">Login</a></li>
+                    <li><a href="/enterprise/register-my-login/">Create Account</a></li>
+                    <li>
+                        <a href="/web/aboard-study-home/">Promote Colleges</a></li>
+                    <li><a href="/enterprise/contact_us_form/" target="_blank">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3 col-md-3">
+        <div class="box transparent">
+            <div class="box-header" style="padding-left: 10%">
+                <h2 class="titlefooter">About</h2>
+            </div>
+            <div class="box-body">
+                <ul class="list-item" style="list-style-type: none">
+                    <li>About Mero Shiksya</li>
+                    <li>Article Writer</li>
+                    <li>Careers</li>
+                    <li>Team</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3 col-md-3" >
+        <div class="box transparent">
+            <div class="box-header" style="padding-left: 10%">
+                <h2 class="titlefooter">Guidance</h2>
+            </div>
+            <div class="box-body">
+                <ul class="list-item" style="list-style-type: none">
+                    <li>News and Articles</li>
+                    <li>Career Councelling</li>
+                    <li>Test Preparation</li>
+                    <li>Entrance Preparation</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3 col-md-3">
+        <div class="box transparent">
+            <div class="box-header" style="padding-left: 10%">
+                <h2 class="titlefooter">Shiksha</h2>
+            </div>
+            <div class="box-body">
+                <ul class="list-item" style="list-style-type: none">
+                    <li>About Us</li>
+                    <li>Careers</li>
+                    <li>Corporate Blog</li>
+                    <li>Shiksha Authors</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div> <!-- ./ Footer Top Row -->
+<!-- // Footer Bottom Row -->
+<div id="fb-root"></div>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+<div class="row" style="background:#292929; color:#737373; font-size: smaller; padding-top: 5%; padding-bottom: 2%">
+    <div class="col-sm" style="padding-top: 3%; padding-left: 5%">
+        <p style="padding-top: 3%">MEROSHIKSHYA.COM<br>©2018</p>
+
+    </div>
+    <div class="col-sm">
+        <img src="logo_edited.png" style="height: 100px; width: auto">
+    </div>
+    <div class="col-sm" style="padding-top: 5%; float: bottom;">
+        <ul class="list-unstyled list-inline social" style="float: left; padding-left: 30%">
+            <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-facebook"></i></a></li>
+            <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-google-plus"></i></a></li>
+        </ul>
+    </div>
+</div> <!-- ./ Footer Bottom Row -->
+</body>
+</html>
